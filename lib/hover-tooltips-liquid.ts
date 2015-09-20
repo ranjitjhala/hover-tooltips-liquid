da0fc37eb88b5f9f202eac72bfc5e44963fd2a4a
Ranjit Jhala <jhala@cs.ucsd.edu>
2015-09-20 15:37:36 -0700

    asd

--- lib/hover-tooltips-liquid.ts
+++ lib/hover-tooltips-liquid.ts
@@ -1,13 +1,17 @@
-/// <reference path="../typings/hover.d.ts" />
+/// <reference path="../typings/tsd.d.ts" />
 
-import {Annotations, debugLiquidUtils}  from 'liquid-utils'
-import {HoverTooltips, provider, debug} from 'hover-tooltips';
+import {Annotations, debugLiquidUtils}  from './liquid-utils'
+import {HoverTooltips, provider, debug} from './hover-tooltips';
 
-var annotProvider = new Annotations('liquid', [], '.liquid');
+var annProvider = new Annotations('liquid', [], '.liquid');
 
 export class HdevtoolsTooltips extends HoverTooltips {
-  provider:Hover.IProvider = provider(hdevtoolsProvider);
   syntax = 'source.haskell'
+
+  provider:Hover.IProvider = (p:Hover.Position, s:string) => {
+    return annProvider.getInfo(p, s);
+  }
+
 }
 
 module.exports = new HdevtoolsTooltips();
