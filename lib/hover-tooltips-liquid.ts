71d175b5c24fd277987f2cf7c3d5c4007e1535f8
Ranjit Jhala <jhala@cs.ucsd.edu>
2015-09-20 15:17:50 -0700

    moar debug

--- lib/hover-tooltips-liquid.ts
+++ lib/hover-tooltips-liquid.ts
@@ -1,56 +1,13 @@
 /// <reference path="../typings/hover.d.ts" />
 
+import {Annotations, debugLiquidUtils}  from 'liquid-utils'
 import {HoverTooltips, provider, debug} from 'hover-tooltips';
 
-function hdevtoolsResult(msg:string[]):string{
-  try {
-   // debug("PARSE (text): " + msg + "\nJSON: " + JSON.stringify(msg));
-   var ls = msg[0].split('\n');
-   // debug("PARSE (ls): " + ls);
-   var l0 = ls[0];
-   // debug("PARSE (l0): " + l0);
-   // e.g. text = 12 10 12 14 "Int -> Int"
-   var ty = l0.match(/\".*\"/i);
-   // debug("PARSE (ty): " + ty);
-   // e.g  ty   = "Int -> Int"
-   var n  = ty[0].length;
-   // e.g res   = Int -> Int
-   var res = ty[0].substring(1, n-1);
-   // debug("PARSE (res): " + res);
-   return res;
- } catch (e) {
-   // debug("PARSE ERROR: " + e.toString());
-   return null;
-}
-}
-
-function hdevtoolsCommand(p:Hover.Position):Hover.Command {
-  var execP   = atom.config.get('hover-tooltips-hdevtools.executablePath');
-  var socketP = atom.config.get('hover-tooltips-hdevtools.socketPath');
-  return { cmd: execP
-         , args: [ 'type'
-                 , '-s', socketP
-                 , `${p.file}`, `${p.line}`, `${p.column}`
-                 ]
-         };
-}
-
-var hdevtoolsProvider : Hover.Provider = {
-    result: hdevtoolsResult
-  , command: hdevtoolsCommand
-  }
+var annotProvider = new Annotations('liquid', [], '.liquid');
 
 export class HdevtoolsTooltips extends HoverTooltips {
-
   provider:Hover.IProvider = provider(hdevtoolsProvider);
-
   syntax = 'source.haskell'
-
-  config = { executablePath: { type: 'string'
-                             , default: 'hdevtools'}
-           , socketPath    : { type: 'string'
-                             , default: '~/.hdevtools-socket-atom'}
-           }
 }
 
 module.exports = new HdevtoolsTooltips();
---
+++ typings/liquid-utils.d.ts
@@ -0,0 +1,31 @@
+/// <reference path="../typings/hover.d.ts" />
+import Q = require('q');
+export declare var debugLiquidUtils: any;
+export declare type imap<A> = {
+    [x: number]: A;
+};
+export interface Annot {
+    ident: string;
+    ann: string;
+    row: number;
+    col: number;
+}
+export declare type TypeInfo = imap<imap<Annot>>;
+export interface LiquidInfo {
+    status: string;
+    types: TypeInfo;
+    errors: Hover.Error[];
+}
+export declare class Annotations {
+    private fileMap;
+    private cmd;
+    private args;
+    private subDir;
+    constructor(cmd: string, args: string[], subDir: string);
+    private command(sFile);
+    private jsonFile(srcFile);
+    private rebuild(sFile, sTime);
+    private refresh(sFile);
+    getInfo(p: Hover.Position): Q.Promise<Hover.Info>;
+    getErrors(f: string): Q.Promise<Hover.Error[]>;
+}
